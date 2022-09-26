71e0a52891042486b8129a8ea63ee236067fd1f5
“Edu58” <edumuriithi58@gmail.com>
2022-09-26 13:04:27 +0300

    matching brackets

---
+++ bracketMatching.js
@@ -0,0 +1,48 @@
+const bracketsValid = (brackets) => {
+    const stack = []
+
+    for (let i = 0; i < brackets.length; i++) {
+
+        let x = brackets[i]
+
+        if (x == '(' || x == '{' || x == '[') {
+            stack.push(x)
+            continue
+        }
+
+        if (stack.length == 0) {
+            return false
+        }
+
+        let check;
+        switch (x) {
+            case ')':
+                check = stack.pop()
+                if (check == '{' || check == '[') {
+                    return false
+                }
+                break
+            case '}':
+                check = stack.pop()
+                if (check == '(' || check == '[') {
+                    return false
+                }
+                break
+            case ']':
+                check = stack.pop()
+                if (check == '(' || check == '{') {
+                    return false
+                }
+                break
+        }
+    }
+
+    return (stack.length == 0)
+}
+
+
+if (bracketsValid('({[[]]})')) {
+    console.log('balanced')
+} else {
+    console.log('NOT balanced')
+}
